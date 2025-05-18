0025ad0efaf8b9e9e288ca9d11c94c8e5ab0b23b
Soha Khan <jahanzeb.soha@gmail.com>
2025-05-18 21:57:49 +0500

    [Video #6] arrow attack added

--- script/player.gd
+++ script/player.gd
@@ -5,6 +5,10 @@ var player_state
 
 @export var ivn: Ivn
 
+var bow_equiped = false
+var bow_cooldown = true
+var arrow =  preload("res://scene/arrow.tscn")
+
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	
@@ -15,6 +19,25 @@ func _physics_process(delta):
 	
 	velocity = direction * speed
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("e"):
+		if bow_equiped:
+			bow_equiped = false
+		else:
+			bow_equiped = true
+	
+	var mouse_pos = get_global_mouse_position()
+	$Marker2D.look_at(mouse_pos)
+	
+	if Input.is_action_just_pressed("left_mouse") and bow_equiped and bow_cooldown:
+		bow_cooldown = false
+		var arrow_instance = arrow.instantiate()
+		arrow_instance.rotation = $Marker2D.rotation
+		arrow_instance.global_position = $Marker2D.global_position
+		add_child(arrow_instance)
+		
+		await get_tree().create_timer(0.4).timeout
+		bow_cooldown = true
 	
 	play_anim(direction)
 	
