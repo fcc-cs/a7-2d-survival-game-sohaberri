851cba276e6ce4d6841618413e7838fecb9ead20
Soha Khan <jahanzeb.soha@gmail.com>
2025-05-18 15:39:13 +0500

    [Video #3] full apple tree

--- script/apple_tree.gd
+++ script/apple_tree.gd
@@ -3,6 +3,7 @@ extends Node2D
 var state = "no apples" # no apples, apples
 var player_in_area = false
 
+var apple = preload("res://scene/apple_collectible.tscn")
 func _ready():
 	if state == "no apples":
 		$growth_timer.start()
@@ -14,7 +15,7 @@ func _process(delta):
 		$AnimatedSprite2D.play("apples")
 	if player_in_area and Input.is_action_just_pressed("e"):
 		state = "no apples"
-		$growth_timer.start()
+		drop_apple()
 
 
 func _on_pickable_area_body_entered(body: Node2D):
@@ -30,3 +31,11 @@ func _on_pickable_area_body_exited(body: Node2D):
 func _on_growth_timer_timeout():
 	if state == "no apples":
 		state = "apples"
+
+func drop_apple():
+	var apple_instance = apple. instantiate()
+	apple_instance.global_position = $Marker2D.global_position
+	get_parent().add_child(apple_instance)
+	
+	await get_tree().create_timer(3).timeout
+	$growth_timer.start()
