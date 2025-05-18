b709597d055f0fd736825648c0f9a251f8750da4
Soha Khan <jahanzeb.soha@gmail.com>
2025-05-18 20:46:32 +0500

    [Video #5] stick added + inv fully functional

--- inventory/ivn_ui_slot.gd
+++ inventory/ivn_ui_slot.gd
@@ -7,9 +7,10 @@ func update(slot: IvnSlot):
 	if !slot.item:
 		item_visual.visible = false
 		amount_text.visible = false
-		
+
 	else:
 		item_visual.visible = true
 		item_visual.texture = slot.item.texture
-		amount_text.visible = true
+		if slot.amount>1:
+			amount_text.visible = true
 		amount_text.text = str(slot.amount)
